015099a0e18396ab4dea2c87acf99b44bcf0546e
Joe Ellis <ellis125@gmail.com>
2016-01-24 20:04:07 -0800

    Start serving assets

--- base-api/src/main/java/org/base/BaseService.java
+++ base-api/src/main/java/org/base/BaseService.java
@@ -10,7 +10,7 @@ import javax.ws.rs.core.MediaType;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-@Path("/api")
+@Path("/base")
 public interface BaseService {
 
     @GET
--- base/src/main/java/org/base/BaseServer.java
+++ base/src/main/java/org/base/BaseServer.java
@@ -1,18 +1,24 @@
 package org.base;
 
 import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
 public class BaseServer extends Application<ServerConfig> {
 
-    public static void main(String[] args) throws Exception {
-        new BaseServer().run(args);
+    public void initialize(Bootstrap<ServerConfig> bootstrap) {
+        bootstrap.addBundle(new AssetsBundle("/assets", "/", "index.html"));
     }
 
     @Override
     public void run(ServerConfig configuration, Environment environment) throws Exception {
         BaseResource resource = new BaseResource();
         environment.jersey().register(resource);
+    }
+
+    public static void main(String[] args) throws Exception {
+        new BaseServer().run(args);
     }
 
 }
